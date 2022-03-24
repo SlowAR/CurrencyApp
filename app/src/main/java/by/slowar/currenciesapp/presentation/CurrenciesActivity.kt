e5df6ccfc4425bce517a0d4b3dbe0f8102457e71
SlowAR <antonmbulat@gmail.com>
2022-03-24 07:26:04 +0300

    add bottom nav menu

--- app/src/main/java/by/slowar/currenciesapp/presentation/CurrenciesActivity.kt
+++ app/src/main/java/by/slowar/currenciesapp/presentation/CurrenciesActivity.kt
@@ -2,7 +2,10 @@ package by.slowar.currenciesapp.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import by.slowar.currenciesapp.R
 import by.slowar.currenciesapp.databinding.ActivityCurrenciesBinding
+import by.slowar.currenciesapp.presentation.currencieslist.CurrenciesListFragment
 
 class CurrenciesActivity : AppCompatActivity() {
 
@@ -12,6 +15,31 @@ class CurrenciesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCurrenciesBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         setSupportActionBar(binding.toolbar)
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.currenciesListItem -> {
+                    switchFragment(CurrenciesListFragment.newInstance())
+                    true
+                }
+                R.id.favouritesCurrenciesItem -> {
+                    //switchFragment(currencyConverterFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.bottomNavigation.selectedItemId = R.id.currenciesListItem
+    }
+
+    private fun switchFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .setReorderingAllowed(true)
+            .replace(R.id.fragmentContainerView, fragment)
+            .commit()
     }
 }
